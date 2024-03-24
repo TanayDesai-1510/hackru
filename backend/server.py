7b7fb04b3a54aec2f51883639c0a4074540f01cf
Tanay Desai <tanaydesai2004@gmail.com>
2024-03-24 12:27:42 -0400

    final2

--- backend/server.py
+++ backend/server.py
@@ -18,7 +18,6 @@ from more.hackru import (
 app = Flask(__name__)
 
 FINAL = pd.read_csv("C:\\Users\\tanay\\OneDrive\\Desktop\\hackru\\hackru\\backend\\more\\final_student_course_records.csv")
-KEY = "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"
 
 @app.route("/classtrial")
 def hello():
